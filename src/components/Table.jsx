35f4f58919026cf9af323964d2f434a3fe6d9910
Kizito007 <nwakakelechi49@gmail.com>
2022-09-07 00:56:24 +0100

    home screen proto

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
 import Table from '@mui/material/Table';
+import Button from '@mui/material/Button';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import pdf from "../assets/pdf.svg" 
+import xls from "../assets/xls.svg" 
+import triangle from "../assets/triangle.svg" 
+import Lens from "@mui/icons-material/Lens"
+import CheckCircle from "@mui/icons-material/CheckCircle"
+import MoreVert from "@mui/icons-material/MoreVert"
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(sn, text, servers, time, health) {
+  return { sn, text, servers, time, health };
 }
 
 const rows = [
@@ -22,39 +31,65 @@ const rows = [
 export default function BasicTable() {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table>
         <TableHead>
         <TableRow>            
-          <TableCell align="right"> Showing 10 of 5000 Entries</TableCell>
+          <TableCell sx={{ float: "right" }}> 
+            <img src={pdf}/> <img src={xls} style={{margin: "0 20px"}} />
+            Showing 10 of 5000 Entries
+          </TableCell>
         </TableRow>
         </TableHead>
+      </Table>
+
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
-          <TableRow>
-            <TableCell>S/N</TableCell>
-            <TableCell>APPLICATION</TableCell>
-            <TableCell align="right">SERVERS MAPPED</TableCell>
-            <TableCell align="right">LAST SCAN DATE</TableCell>
-            <TableCell align="right">STATUS</TableCell>
-            <TableCell align="right">ACTION</TableCell>
+          <TableRow sx={{ backgroundColor: "#f1f3f5", color: "#222222" }}>
+            <TableCell> <b> S/N </b> </TableCell>
+            <TableCell></TableCell>
+            <TableCell> <b>APPLICATION</b> </TableCell>
+            <TableCell> <b> SERVERS MAPPED</b></TableCell>
+            <TableCell> <b>LAST SCAN DATE</b> </TableCell>
+            <TableCell> <b>STATUS</b> </TableCell>
+            <TableCell> <b>ACTION</b> </TableCell>
+            <TableCell></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
-              key={row.name}
+              key={row.sn}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.sn}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell>
+                <Lens sx={{ color: "#14DB77", }}>add_circle</Lens> 
+              </TableCell>
+              <TableCell>
+                {row.text}
+              </TableCell>
+              <TableCell sx={{ color: "#499DFF", }}>{row.servers}</TableCell>
+              <TableCell>{row.time}</TableCell>
+              <TableCell>
+              <CheckCircle sx={{ color: "#14DB77", }}>add_circle</CheckCircle> 
+              </TableCell>
+              <TableCell>
+                <Button variant="contained" sx={{backgroundColor: "#499DFF", py: 0.5, }}>
+                  View Result
+                </Button>
+              </TableCell>
+              <TableCell>
+              <MoreVert sx={{ color: "#f1f3f5", }}>add_circle</MoreVert> 
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
-      </Table>
+      </Table><br/><br/>
+      <Stack spacing={2}>
+        <Pagination count={1539} color="primary" />
+      </Stack><br/><br/>
     </TableContainer>
   );
 }
